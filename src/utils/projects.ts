f3b7445a8c324c8e507302ef6ce2176069038443
Dorian URBANIAK <dorian.urbaniak@gmail.com>
2024-03-02 03:25:06 +0100

    [SECTION 2] text correction

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -11,7 +11,7 @@ export const projects = [
 	{
 		title: "Majore",
 		description:
-			"Majore est un projet que j'ai réalisé dans le cadre de mon alternance chez Strat Inc. Ce site permet à des recruteurs et des demandeurs d'emploi saisonnier de trouver leur offre/candidat idéal. Le site a été réalisé avec React.js, Node.js (express) et Firebase. J'ai codé le front et le back dans leur intégralité, ainsi qu'une partie algorithme pour trouver les correspondances idéales entre les offres et les candidats.",
+			"Majore est un projet que j'ai réalisé dans le cadre de mon activité chez Strat Inc. Ce site permet à des recruteurs et des demandeurs d'emploi saisonnier de trouver leur offre/candidat idéal. Le site a été réalisé avec React.js, Node.js (express) et Firebase. J'ai codé le front et le back dans leur intégralité, ainsi qu'une partie algorithme pour trouver les correspondances idéales entre les candidats et les offres.",
 		desktopImage: majoreDesktop,
 		phoneImage: majorePhone,
 		link: "https://app.majore.co",
@@ -19,7 +19,7 @@ export const projects = [
 	{
 		title: "Koacher",
 		description:
-			"Koacher est un projet que j'ai réalisé en compagnie de deux autres alternants dans le cadre de mon alternance chez Alter One. Ce site applicatif encore en développement permettera de trouver son coach sportif idéal, et pour les coachs, de gérer leur emploi du temps. Ce projet est réalisé à l'aide de React.js, Node.js (express), Firebase. Je me suis occupé d'une partie du front et du back ainsi que de la supervision globale du projet.",
+			"Koacher est un projet que j'ai réalisé dans le cadre de mon activité chez Alter One. Ce site applicatif encore en développement permettra de trouver son coach sportif idéal, et pour les coachs, de gérer leur emploi du temps. Ce projet est réalisé à l'aide de React.js, Node.js (express), Firebase. Je me suis occupé d'une partie du front et du back ainsi que de la supervision globale du projet.",
 		desktopImage: koacherDesktop,
 		phoneImage: koacherPhone,
 		link: "https://koacher.app",
@@ -27,7 +27,7 @@ export const projects = [
 	{
 		title: "Jean-Luc URBANIAK",
 		description:
-			"Le site internet de Jean-Luc URBANIAK est un projet réalisé pour l'activitée professionnelle de psychotérapeute de mon père. Je l'ai entièrement codé à l'aide de React.js et des appels API de Firebase. Il est actuellement mis en ligne via Vercel",
+			"Le site internet de Jean-Luc URBANIAK est un projet réalisé pour l'activité professionnelle de psychothérapeute de mon père. Je l'ai entièrement codé à l'aide de React.js et des appels API de Firebase. Il est actuellement mis en ligne via Vercel",
 		desktopImage: urbaniakDesktop,
 		phoneImage: urbaniakPhone,
 		link: "https://jeanlucurbaniak.fr",
@@ -35,7 +35,7 @@ export const projects = [
 	{
 		title: "Dorian URBANIAK",
 		description:
-			"Voici une petite mise en abime pour vous parler de mon site. Mon site à été entièrement designé et codé par mes soin et à l'aide de Svelte. C'est un language que je voulais pratiquer et apprendre depuis longtemps et qui me permet de faire des sites dynamiques tout en étant beaucoup plus léger et rapide que React à mettre en place.",
+			"Voici une petite mise en abime pour vous parler de mon site. Le design et le code de mon site ont été entièrement réalisés par mes soins et à l'aide de Svelte. C'est un langage que je voulais pratiquer et apprendre. Il permet de faire des sites dynamiques comme React tout en étant beaucoup plus léger et rapide à mettre en place.",
 		desktopImage: dorianDesktop,
 		phoneImage: dorianPhone,
 	},
